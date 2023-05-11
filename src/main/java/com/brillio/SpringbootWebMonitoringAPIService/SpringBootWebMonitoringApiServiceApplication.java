38df926ace7042ce455ae6e20e492bab9833095b
Preeti Mathpati <Preeti.Mathpati@BRILLIO.COM>
2023-05-11 13:23:17 +0530

    Initial commit

---
+++ src/main/java/com/brillio/SpringbootWebMonitoringAPIService/SpringBootWebMonitoringApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.brillio.SpringbootWebMonitoringAPIService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebMonitoringApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebMonitoringApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brillio/SpringbootWebMonitoringAPIService/SpringBootWebMonitoringApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brillio.SpringbootWebMonitoringAPIService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebMonitoringApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
